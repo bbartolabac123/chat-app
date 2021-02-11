21f138ed41e397756b35ee46416cb3ccd1ea1953
Christine Diane Malazarte <malazarte.cde@sprobe.com>
2021-02-11 19:22:54 +0800

    Initial Commit

---
+++ chatapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatapp
+//
+//  Created by Diane Malazarte on 2/11/21.
+//  Copyright © 2021 Benjamin Bartolabac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
